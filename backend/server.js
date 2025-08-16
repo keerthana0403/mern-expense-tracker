75228e75b18f71decbaf3f4a30126802eef11adb
keerthana e <keerthanae0403@outlook.com>
2025-08-16 23:41:21 +0530

    changed folder structure

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/expense-record", expenseRoutes);
 
 // Serve static frontend files
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
 });
 
 // Start server
