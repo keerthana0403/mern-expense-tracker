f9b89b24a9149a5d95048f13de0bb2c9840f846d
keerthana e <keerthanae0403@outlook.com>
2026-02-10 14:00:24 +0530

    refactored backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import expenseRoutes from "./routes/expense.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-
-const __dirname = path.resolve();
 
 dotenv.config();
 const app = express();
@@ -17,19 +15,25 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 // API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/expense-record", expenseRoutes);
 
-// Serve static frontend files
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+// Health check
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
+
+connectToMongoDB();
 
 // Start server
 app.listen(PORT, () => {
-  connectToMongoDB();
   console.log(`Server running on http://localhost:${PORT}`);
 });
